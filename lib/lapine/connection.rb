91abb13633358c6fd38171be72aca6ffa0ad755c
Eric Saxby & Jo Pu <eric+jo@wanelo.com>
2014-11-10 12:05:19 -0800

    Connection name and exchange type as Connection attributes
    Ensure that we retain these properties on the Lapine::Connection
    object, for when we try to reconnect later.

--- lib/lapine/connection.rb
+++ lib/lapine/connection.rb
@@ -2,11 +2,13 @@ require 'bunny'
 
 module Lapine
   class Connection
-    attr_reader :conn, :name, :props
+    attr_reader :conn, :name, :props, :connection_name, :exchange_type
 
     def initialize(name, properties)
       @name = name
       @props = properties.dup
+      @connection_name = props.delete(:connection)
+      @exchange_type = props.delete(:type)
     end
 
     def exchange
@@ -15,11 +17,10 @@ module Lapine
     end
 
     def reconnect
-      connection_props = Lapine.config.connection_properties[props.delete(:connection)]
+      connection_props = Lapine.config.connection_properties[connection_name]
       @conn = Bunny.new(connection_props)
       conn.start
       channel = conn.create_channel
-      exchange_type = props.delete(:type)
       @exchange = Bunny::Exchange.new(channel, exchange_type, name, props)
     end
   end
